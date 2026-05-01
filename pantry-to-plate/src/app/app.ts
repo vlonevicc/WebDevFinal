9c21a2c240f2fbb771ea305a6227cfcd703dc1e0
sblick <sblick@fibersmith.co>
2026-05-01 12:25:52 -0500

    added other base pages, top bar and icon

--- pantry-to-plate/src/app/app.ts
+++ pantry-to-plate/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterLinkActive } from '@angular/router';
+import { Topbar } from './components/topbar/topbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, CommonModule, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, CommonModule, RouterLinkActive, Topbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
