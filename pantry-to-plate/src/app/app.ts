fb812e310e80924facb7f15f379b25d2b045f208
sblick <sblick@fibersmith.co>
2026-05-01 11:41:25 -0500

    cookbook page done and added a favicon

--- pantry-to-plate/src/app/app.ts
+++ pantry-to-plate/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, CommonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
